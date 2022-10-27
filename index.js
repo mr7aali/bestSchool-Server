d9ca53c73d517e259c50ba62e8d79ea2182f13ac
mr7aali <mr7aali@gmail.com>
2022-10-27 16:12:46 +0600

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 app.use(cors())
 const port = 5000
 
-const courses= require('./data/catagories.json')
+const courses = require('./data/catagories.json')
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -12,6 +12,13 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
   res.send(courses)
+})
+app.get('/courses/:_id', (req, res) => {
+  const ID= req.params._id;
+  console.log(req.params._id)
+  const wnatedCourse =  courses.find(c=>c.id === ID);
+  res.send(wnatedCourse)
+
 })
 
 
